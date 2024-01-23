a75b95faa9b37d4ceaa325ad6bdbb63869278f21
Daniel Afonso <daniel.afonso@parceiros.nos.pt>
2024-01-23 22:01:26 +0000

    Add Loader script with cleaner mode

---
+++ cmd/loader/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"cargoship/internal/configurations"
+	"cargoship/internal/files"
+	"cargoship/internal/logging"
+	"cargoship/internal/manifests"
+)
+
+var (
+	scriptLogger logging.Logger
+	filesLogger  logging.Logger
+)
+
+func compressFile(filepath string) error {
+	// compress files
+	fmt.Printf("compress %s\n", filepath)
+	return nil
+}
+
+func cleanFile(filepath string) error {
+	// clean files
+	err := os.Remove(filepath)
+	if err == nil {
+		scriptLogger.LogInfo(fmt.Sprintf("Deleted file %s\n", filepath))
+	}
+	return err
+}
+
+func main() {
+	// command line Flags
+	var configFilepath string
+	flag.StringVar(&configFilepath, "config", "loader_config.yaml", "Path to configuration yaml")
+	flag.StringVar(&configFilepath, "c", "loader_config.yaml", "")
+	flag.Usage = func() {
+		fmt.Print(`Usage of shipper:
+  -c, --config  path to configuation yaml 
+  -h, --help    display this help message
+`)
+	}
+	flag.Parse()
+
+	// read script configuration file
+	configs, err := configurations.LoaderReadConfig(configFilepath)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// start loggers
+	scriptLogger.Init(configs.Log.Script, configs.Log2Console)
+	filesLogger.Init(configs.Log.Files, configs.Log2Console)
+	defer scriptLogger.Close()
+	defer filesLogger.Close()
+
+	// read load manifest
+	manifest, err := manifests.LoaderReadTimes(configs.TimesPath)
+	if err != nil {
+		scriptLogger.LogError(err.Error())
+		panic(err)
+	}
+
+	// defer update/write manifest file
+	defer manifests.LoaderWriteTimes(&manifest, configs.TimesPath)
+
+	// process services
+	for _, service := range configs.Services {
+		scriptLogger.LogInfo(fmt.Sprintf("Processing service: %s\n", service.Name))
+		var processFile func(string) error
+		if service.Mode == "compress" {
+			// set processFile function to compressFile
+			processFile = compressFile
+			files.CheckLocalFolder(service.Dst)
+		} else if service.Mode == "cleaner" {
+			// set processFile function to cleanFile
+			processFile = cleanFile
+		} else {
+			scriptLogger.LogWarn(fmt.Sprintf("ERROR: Unkown mode, %s, on service %s.\n", service.Mode, service.Name))
+			continue
+		}
+
+		// list local files
+		files2Process, err := files.ListLocalDirectory(service.Src, service.Prefix, service.Extension)
+		if err != nil {
+			scriptLogger.LogError(err.Error())
+			log.Panic(err)
+		}
+		// get last process file
+		lastFile := manifests.LoaderGetTimes(&manifest, service.Name, service.Mode)
+		files2Process = files.DateFilterLocalDirectory(files2Process, lastFile, service.MaxTime, service.Window)
+		lastFileProcess := lastFile
+		for _, file := range files2Process {
+			filepath := fmt.Sprintf("%s/%s", service.Src, file.Name())
+			// if compress compress
+			err = processFile(filepath)
+			if err != nil {
+				scriptLogger.LogError(err.Error())
+			}
+			//update
+			lastFileProcess = file.ModTime()
+		}
+
+		// update last process file
+		if lastFileProcess != lastFile {
+			manifests.LoaderUpsertTimes(&manifest, service.Name, service.Mode, lastFileProcess)
+		}
+	}
+}
